2f644b6c8ad0bc0afd6c05a45e246e2139a15836
ranjurave <39932642+ranjurave@users.noreply.github.com>
2022-05-17 16:30:32 +1200

    some work

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
-console.log("This is a message from external js")
-const HighScore = 456
-console.log(HighScore)
+const str1 = '\"Concentrate all your thoughts';
+const str2 = ' upon the work in hand. The sun\'s rays do not burn';
+const str3 = ' until brought to a focus.\"';
+const str4 = ' -Alexander Greham Bell';
+
+let fullStr = str1+str2+str3+str4;
+console.log(fullStr);
+console.log(fullStr.length);
+str5=str1.toUpperCase();
+console.log(str5)
+
+let message1 = prompt('Enter something','anything is fine');
+alert(message1+10);
