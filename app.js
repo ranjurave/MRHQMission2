c08bdabd04ece879788dba966195d1a2535156e8
ranjurave <ranjurave@gmail.com>
2022-05-26 23:34:41 +1200

    js added

---
+++ app.js
@@ -0,0 +1,15 @@
+function changeopacityover(works_text){
+    works_text.style.opacity = '100%';
+}
+function changeopacityout(works_text){
+    works_text.style.opacity = '0%';
+}
+function text_shadow_on(text){
+    text.style.color = "#1182dd";
+    //text.style.text_shadow = '2px 2px #ff0000';
+    //text.textin.style.text_shadow = '2px 2px #ff0000';
+}
+function text_shadow_off(text){
+    text.style.color = 'white';
+    //text.textin.style.text_shadow = '0px 0px #ff0000';
+}
