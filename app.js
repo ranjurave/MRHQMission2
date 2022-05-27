e7a2091e373b9c73571d63d8431e72bda336528c
ranjurave <ranjurave@gmail.com>
2022-05-27 18:47:36 +1200

    some change

--- app.js
+++ app.js
@@ -6,10 +6,7 @@ function changeopacityout(works_text){
 }
 function text_shadow_on(text){
     text.style.color = "#1182dd";
-    //text.style.text_shadow = '2px 2px #ff0000';
-    //text.textin.style.text_shadow = '2px 2px #ff0000';
 }
 function text_shadow_off(text){
     text.style.color = 'white';
-    //text.textin.style.text_shadow = '0px 0px #ff0000';
 }
