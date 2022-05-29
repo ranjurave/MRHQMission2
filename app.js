91f0d90aef9f733a36ae57eb0ddc6af2433949f1
ranjurave <ranjurave@gmail.com>
2022-05-29 22:38:21 +1200

    animation added

--- app.js
+++ app.js
@@ -1,3 +1,35 @@
+// const hi_text = document.getElementById('hi');
+// const ranju_text = document.getElementById('IamRanju');
+const intro_text = document.getElementById('selfIntro');
+// const strHiText = hi_text.textContent;
+// const strRanjuText = ranju_text.textContent;
+// const strIntroText = intro_text.textContent;
+const strText = intro_text.textContent;
+const splitText = strText.split("");
+intro_text.textContent="";
+
+for(let i = 0; i < splitText.length; i++){
+    intro_text.innerHTML += '<span>'+splitText[i]+'<span>'
+}
+
+let char = 0;
+let timer = setInterval(onTick, 50);
+function onTick(){
+    const span = intro_text.querySelectorAll('span')[char];
+    span.classList.add('fade');
+    char++;
+    console.log(char);
+    if(char>=splitText.length*2){
+        complete();
+        return;        
+    }
+}
+
+function complete(){
+    clearInterval(timer);
+    timer = null;
+}
+
 function changeopacityover(works_text){
     works_text.style.opacity = '100%';
 }
